0f990223da88d39e816bac1f0216ba6d1a1abaf1
PutraSudaryanto <putra.sudaryanto@gmail.com>
2015-01-02 15:42:11 +0700

    12111075 :: modify main page

--- 12111075/application/controllers/Site.php
+++ 12111075/application/controllers/Site.php
@@ -1,27 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Site extends CI_Controller {
+class Site extends CI_Controller
+{	
+	function _template($view, $data = NULL)
+	{
+		$this->load->view('layouts/header', $data);
+		$this->load->view($view, $data);
+		$this->load->view('layouts/footer', $data);
+	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		$this->load->view('/site/front_index');
+		$data['pageTitle'] = '';
+		$data['pageDescription'] = '';
+		$data['pageMeta'] = '';
+		$this->_template('/site/front_index', $data);
 	}
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
